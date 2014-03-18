549830fb4979496e92ae077d40b57d48182dffca
Aleksander Grzyb <aleksander.grzyb@gmail.com>
2014-03-18 20:49:57 +0100

    Initial Commit

---
+++ FaceDetection/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FaceDetection
+//
+//  Created by Aleksander Grzyb on 18/03/14.
+//  Copyright (c) 2014 PIRO. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
